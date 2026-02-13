e67dcf0d0dad4d099ad4e9ba61bfddbc1a26ba39
Anusha Maheshwaran <anumahi127@gmail.com>
2026-02-13 12:12:27 +0530

    Assignment1

---
+++ javascript-training/sumoftwo-numbers.js
@@ -0,0 +1,12 @@
+//let a = 10.5;
+//let b = 20;
+//let sum = a + b;
+//console.log("The Sum Of Numbers is:", sum);
+
+function addnumber(a,b)
+{
+    return a*b;
+}
+
+let result = addnumber(10,20);
+console.log ("Sum is:", result);
